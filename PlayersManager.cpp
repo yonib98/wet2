58d5250d1f17072f186840cd34a498339c8ea809
IvriHikri <ivrihikri@campus.technion.ac.il>
2022-01-08 00:44:40 +0200

    remove and 2Increases updated

--- PlayersManager.cpp
+++ PlayersManager.cpp
@@ -1,5 +1,5 @@
 #include "PlayersManager.h"
-PlayerManager::PlayerManager(int k,int scale): k(k),scale(scale),players_table(initial_size),groups_ids(k){
+PlayerManager::PlayerManager(int k,int scale): all_players_level_tree(false,scale),k(k),scale(scale),players_table(initial_size),groups_ids(k){
     zero_level_scores=new int[scale+1];
     for(int i=1;i<=scale;i++){
         zero_level_scores[i]=0;
@@ -21,10 +21,67 @@ void PlayerManager::addPlayer(int player_id,int group_id,int score){
    groups_array[real_group_id].group_zero_level_scores[score]++;
 
 }
-void removePlayer(int player_id);
 
-void increasePlayerIDLevel(int player_id,int level_increase){
+void PlayerManager::removePlayer(int player_id){
+    Player* player=players_table.search(player_id);
+    int player_level=player->level;
+    int player_score=player->score;
+    int player_group_id=player->group_id;
+    int index_group= groups_ids.find(player_group_id);
+    Group group=groups_array[index_group];
+    if(player_level==0){
+        zero_level_scores[player_score]--;
+        group.group_zero_level_scores[player_score]--;
+    }
+    else
+    {
+        all_players_level_tree.remove(player_level,0,player_score);
+        group.levels_tree.remove(player_level,0,player_score);
+    }
+    players_table.remove(player_id);
+    delete player;
+}
+
+void PlayerManager::increasePlayerIDLevel(int player_id,int level_increase){
     Player* player=players_table.search(player_id);
     int old_level=player->level;
-    
+    int player_group_id=player->group_id;
+    int index_group= groups_ids.find(player_group_id);
+    Group group=groups_array[index_group];
+    player->level+=level_increase;
+    if(old_level==0){
+        zero_level_scores[score]--;
+        all_players_level_tree.insert(player->level,0,player->score);
+        group.group_zero_level_scores[scale]--;
+        group.levels_tree.insert(player->level,0,player->score)
+    }
+    else
+    {
+        all_players_level_tree.remove(old_level,0,player->score);
+        all_players_level_tree.insert(player->level,0,player->score);
+        group.levels_tree.remove(old_level,0,player->score);
+        group.levels_tree.insert(player->level,0,player->score);
+    }
+}
+
+void PlayerManager::changePlayerIDScore(int player_id, int new_score){
+    Player* player=players_table.search(player_id);
+    int player_level=player->level;
+    int old_score=player->score;
+    player->score=new_score;
+    int player_group_id=player->group_id;
+    int index_group= groups_ids.find(player_group_id);
+    Group group=groups_array[index_group];
+    if(player_level==0){
+        zero_level_scores[old_score]--;
+        zero_level_scores[new_Score]++;
+        group.group_zero_level_scores[old_score]--;
+        group.group_zero_level_scores[new_score]++;
+    }
+    else{
+        all_players_level_tree.remove(player_level,0,old_score);
+        all_players_level_tree.insert(player_level,0,new_score);
+        group.levels_tree.remove(player_level,0,old_score);
+        group.levels_tree.insert(player_level,0,new_score);
+    }
 }
--- PlayersManager.h
+++ PlayersManager.h
@@ -3,43 +3,47 @@
 #include "AVLTree.h"
 #include "HashTable.h"
 #include "Union.h"
-class Player{
-    friend class PlayerManager;
-    int player_id;
-    int group_id;
-    int score;
-    int level;
 
-public:
-    Player(int player_id,int group_id,int score,int level=0): player_id(player_id),group_id(group_id),score(score),level(level) {};
-};
-class Group {
-    friend class PlayerManager;
-
-    AVLTree levels_tree;
-    int *group_zero_level_scores;
-    int size;
-    int scale;
-public:
-    Group ()=default;
-    Group(int scale) : levels_tree(false, scale), size(0), scale(scale) {
-        group_zero_level_scores = new int[scale + 1];
-        for (int i = 0; i <= scale; i++) {
-            group_zero_level_scores[i] = 0;
-        }
-    }
-};
 class PlayerManager{
 public:
+    class Player{
+        friend class PlayerManager;
+        int player_id;
+        int group_id;
+        int score;
+        int level;
+
+    public:
+        Player(int player_id,int group_id,int score,int level=0): player_id(player_id),group_id(group_id),score(score),level(level) {};
+    };
+    class Group {
+        friend class PlayerManager;
+
+        AVLTree levels_tree;
+        int *group_zero_level_scores;
+        int size;
+        int scale;
+    public:
+        Group ()=default;
+        Group(int scale) : levels_tree(false, scale), size(0), scale(scale) {
+            group_zero_level_scores = new int[scale + 1];
+            for (int i = 0; i <= scale; i++) {
+                group_zero_level_scores[i] = 0;
+            }
+        }
+    };
+
     PlayerManager(int k,int scale);
     void mergeGroups(int first_group_id,int second_group_id);
     void addPlayer(int player_id,int group_id,int score);
     void removePlayer(int player_id);
     void increasePlayerIDLevel(int player_id,int level_increase);
-    void changePlayerIDScore(int player_id, int new_Score);
+    void changePlayerIDScore(int player_id, int new_score);
     ~PlayerManager();
+
 private:
-   const int initial_size=5;
+    const int initial_size=5;
+    AVLTree all_players_level_tree;
     HashTable<Player*> players_table;
     Group* groups_array;
     Union groups_ids;
